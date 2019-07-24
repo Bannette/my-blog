89a447a9dc71b55842f5fe6cf2efa76bca61f152
Bannette <banetaloo3@gmail.com>
2019-07-24 14:59:57 +0300

    added an index file

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -1,7 +1,9 @@
 package models;
 
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +13,20 @@ public class PostTest {
     public void setUp() throws Exception {
     }
 
+
+
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+
     }
 }
